c268e590e17825b1b5861d31126b44bc090e00f5
Migita Hiroaki <migitahiroaki@gmail.com>
2026-06-18 00:04:27 +0900

    veDUSTを持ってない場合スキップ

--- app.py
+++ app.py
@@ -14,12 +14,15 @@ COOL_DOWN_SEC = 1
 shiny_addresses = restore_addresses()
 
 for p in range(START_PAGE_NUM, MAX_PAGE_NUM):
+    print(p)
     addresses = fetch_addresses(CURRENT_TIDE, p, PAGE_SIZE)
     unchecked_addresses = [a for a in addresses if a not in shiny_addresses]
     sleep(COOL_DOWN_SEC)
     for a in unchecked_addresses:
         vedust_ids = fetch_vedust_ids(CURRENT_TIDE, a)
         sleep(COOL_DOWN_SEC)
-        if check_has_shiny(vedust_ids):
-            print(a)
-            append_address(a)
+        if len(vedust_ids) > 0:
+            if check_has_shiny(vedust_ids):
+                print(a)
+                append_address(a)
+    print("\n")
